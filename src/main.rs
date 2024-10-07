9cd1a43a2c85965c65268bddc974630eab3f6464
razor389 <rgranowski@gmail.com>
2024-10-07 17:41:40 -0500

    setup udp server

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
-#[allow(unused_imports)]
-use std::net::UdpSocket;
-
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+use std::io;
+use tokio::net::UdpSocket;
+#[tokio::main]
+async fn main() -> io::Result<()> {
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    // let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    // let mut buf = [0; 512];
-    //
-    // loop {
-    //     match udp_socket.recv_from(&mut buf) {
-    //         Ok((size, source)) => {
-    //             println!("Received {} bytes from {}", size, source);
-    //             let response = [];
-    //             udp_socket
-    //                 .send_to(&response, source)
-    //                 .expect("Failed to send response");
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error receiving data: {}", e);
-    //             break;
-    //         }
-    //     }
-    // }
+    // Bind to the UDP socket at the specified address (port 2053)
+    let udp_socket = UdpSocket::bind("127.0.0.1:2053").await.expect("Failed to bind to address");
+
+    let mut buf = [0; 512];
+
+    loop {
+        // Receive data from the socket
+        let (amt, src) = udp_socket.recv_from(&mut buf).await?;
+        
+        // Redeclare `buf` as slice of the received data and send reverse data back to the origin
+        let buf = &mut buf[..amt];
+        buf.reverse();
+        
+        // Send the reversed data back to the source
+        udp_socket.send_to(buf, &src).await?;
+        
+        println!("Received data from {} and sent response", src);
+    }
 }
